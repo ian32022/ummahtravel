1bc820d6df7ca48b19f1646ce77603cb8af5d45e
ian32022 <147958431+ian32022@users.noreply.github.com>
2025-12-18 20:29:46 +0700

    up

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,5 +1,5 @@
 <?php
-rotected $routeMiddleware = [
+protected $routeMiddleware = [
     'auth' => \App\Http\Middleware\Authenticate::class,
     'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
     'admin' => \App\Http\Middleware\AdminMiddleware::class,
